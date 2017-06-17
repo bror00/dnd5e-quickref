9632fc57d28fd3575719229ee7ac0da4eb9667a3
bror00 <david@davidmarkerickson.com>
2017-06-17 13:12:33 -0400

    Add files via upload

---
+++ js/data_combat.js
@@ -0,0 +1,52 @@
+data_combat = [
+    {
+        title: "Move",
+        icon: "run",
+        subtitle: "Move a distance up to your speed",
+        description: ".",
+        reference: ".",
+        bullets: [
+            "."
+        ]
+    },
+    {
+        title: "Interact",
+        icon: "snatch",
+        subtitle: "Interact with one object and communicate freely",
+        description: ".",
+        reference: ".",
+        bullets: [
+            "."
+        ]
+    },
+    {
+        title: "Action",
+        icon: "juggler",
+        subtitle: "On your turn yoy may take 1 Action as defined",
+        description: ".",
+        reference: ".",
+        bullets: [
+            "."
+        ]
+    },
+    {
+        title: "Bonus Action",
+        icon: "flying-flag",
+        subtitle: "On your turn you make take 1 Bonus Action as determined by your race/class",
+        description: ".",
+        reference: ".",
+        bullets: [
+            "."
+        ]
+    },
+    {
+        title: "Reaction",
+        icon: "recycle",
+        subtitle: "Move a distance up to your speed",
+        description: ".",
+        reference: ".",
+        bullets: [
+            "."
+        ]
+    }
+]
